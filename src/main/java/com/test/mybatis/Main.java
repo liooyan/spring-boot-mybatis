b3659bf145ef0de3db78fa7ae202fccdeec9d8a0
liy <1064012668@qq.com>
2021-05-28 16:52:15 +0800

    Initial commit

---
+++ src/main/java/com/test/mybatis/Main.java
@@ -0,0 +1,13 @@
+package com.test.mybatis;
+
+/**
+ * @author top.lioyan
+ * @version 1.0
+ * @date 2021/5/28 4:47 下午
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
